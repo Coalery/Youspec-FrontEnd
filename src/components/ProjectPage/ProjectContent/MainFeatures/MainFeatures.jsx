6f6fe158f2f1314d71626f27ec487b487170537b
러리 <doralife12@naver.com>
2021-12-19 18:53:27 +0900

    Refactor: border-radius in MainFeatures Image

--- src/components/ProjectPage/ProjectContent/MainFeatures/MainFeatures.jsx
+++ src/components/ProjectPage/ProjectContent/MainFeatures/MainFeatures.jsx
@@ -6,29 +6,44 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import './MainFeatures.scss';
 
+function FeatureImages({ features }) {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      slidesPerView={'auto'}
+      spaceBetween={30}
+      navigation
+      pagination={{
+        clickable: true,
+      }}
+    >
+      {features.map((imgUrl, idx) => (
+        <SwiperSlide key={idx}>
+          <img src={imgUrl} alt={`Slide${idx}`} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+function FeatureStrings({ features }) {
+  return <div></div>;
+}
+
 function MainFeatures() {
+  const featureImages = [
+    'https://cdn.hkbs.co.kr/news/photo/202104/628798_374207_2710.png',
+    'https://cdn.hkbs.co.kr/news/photo/202104/628798_374207_2710.png',
+    'https://cdn.hkbs.co.kr/news/photo/202104/628798_374207_2710.png',
+  ];
+
+  const featureStrings = [];
+
   return (
     <div id="main-features">
       <Title icon="✨" text="주요 기능" />
-      <Swiper
-        modules={[Navigation]}
-        slidesPerView={'auto'}
-        spaceBetween={30}
-        navigation
-        pagination={{
-          clickable: true,
-        }}
-      >
-        {[
-          'https://cdn.hkbs.co.kr/news/photo/202104/628798_374207_2710.png',
-          'https://cdn.hkbs.co.kr/news/photo/202104/628798_374207_2710.png',
-          'https://cdn.hkbs.co.kr/news/photo/202104/628798_374207_2710.png',
-        ].map((imgUrl, idx) => (
-          <SwiperSlide key={idx}>
-            <img src={imgUrl} alt={`Slide${idx}`} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <FeatureImages features={featureImages} />
+      <FeatureStrings features={featureImages} />
     </div>
   );
 }

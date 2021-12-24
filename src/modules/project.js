ecd78565ec99db46c1c5f731ec45c92742db0aa8
러리 <doralife12@naver.com>
2021-12-24 11:44:55 +0900

    Feat: redux project module

--- src/modules/project.js
+++ src/modules/project.js
@@ -5,9 +5,18 @@ import {
   reducerUtils,
 } from '../lib/asyncUtils';
 
-const GET_FILTERED_PROJECTS = 'GET_FILTERED_PROJECTS';
-const GET_FILTERED_PROJECTS_SUCCESS = 'GET_FILTERED_PROJECTS_SUCCESS';
-const GET_FILTERED_PROJECTS_ERROR = 'GET_FILTERED_PROJECTS_ERROR';
+const GET_PROJECT_BY_ID = 'PROJECT/GET_PROJECT_BY_ID';
+const GET_PROJECT_BY_ID_SUCCESS = 'PROJECT/GET_PROJECT_BY_ID_SUCCESS';
+const GET_PROJECT_BY_ID_ERROR = 'PROJECT/GET_PROJECT_BY_ID_ERROR';
+
+const GET_FILTERED_PROJECTS = 'PROJECT/GET_FILTERED_PROJECTS';
+const GET_FILTERED_PROJECTS_SUCCESS = 'PROJECT/GET_FILTERED_PROJECTS_SUCCESS';
+const GET_FILTERED_PROJECTS_ERROR = 'PROJECT/GET_FILTERED_PROJECTS_ERROR';
+
+export const getProjectById = createPromiseThunk(
+  GET_PROJECT_BY_ID,
+  projectApi.getProjectById
+);
 
 export const getFilteredProjects = createPromiseThunk(
   GET_FILTERED_PROJECTS,
@@ -15,11 +24,19 @@ export const getFilteredProjects = createPromiseThunk(
 );
 
 const initialState = {
+  projectById: reducerUtils.initial(),
   filteredProjects: reducerUtils.initial(),
 };
 
 export default function project(state = initialState, action) {
   switch (action.type) {
+    case GET_PROJECT_BY_ID:
+    case GET_PROJECT_BY_ID_SUCCESS:
+    case GET_PROJECT_BY_ID_ERROR:
+      return handleAsyncActions(GET_PROJECT_BY_ID, 'projectById')(
+        state,
+        action
+      );
     case GET_FILTERED_PROJECTS:
     case GET_FILTERED_PROJECTS_SUCCESS:
     case GET_FILTERED_PROJECTS_ERROR:

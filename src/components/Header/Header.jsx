481ee94ac705339df154d7278c5c7687767b8513
러리 <doralife12@naver.com>
2021-12-27 16:26:24 +0900

    Feat: Add Project Dialog

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom';
 
-import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Add';
 import PersonIcon from '@mui/icons-material/Person';
 
 import './Header.scss';
+import AddProjectDialog from './AddProjectDialog/AddProjectDialog';
+import { useState } from 'react';
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <header className="header">
@@ -17,7 +25,8 @@ function Header() {
           </div>
           <div className="header-right">
             <div className="header-icon-button">
-              <SearchIcon />
+              <AddIcon onClick={() => setOpen(true)} />
+              <AddProjectDialog open={open} onClose={onClose} />
             </div>
             <Link className="header-icon-button" to="/portfolio/Coalery">
               <PersonIcon />

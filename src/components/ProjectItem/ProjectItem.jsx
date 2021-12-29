cdfd969023af90e09463673b5a9801c620ff6bf6
러리 <doralife12@naver.com>
2021-12-29 23:10:30 +0900

    Feat: Route to project when click project item

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -2,24 +2,27 @@ import { Chip } from '@mui/material';
 import './ProjectItem.scss';
 
 function ProjectItem({ item }) {
+  console.log(item);
   return (
-    <div className="project-item-container">
-      <img
-        className="project-item-thumbnail-image"
-        src="https://file.mk.co.kr/meet/neds/2021/06/image_readtop_2021_535745_16226846584668330.jpg"
-        alt="cat"
-      />
-      <div className="project-item-contents">
-        <p className="project-item-title">{item.name}</p>
-        {item.projectTechStacks.map((stack) => (
-          <Chip
-            className="project-item-chip"
-            key={`project-item-${stack.id}`}
-            label={stack.techStack.name}
-          />
-        ))}
+    <a className="project-item-link" href={`/project/${item.id}`}>
+      <div className="project-item-container">
+        <img
+          className="project-item-thumbnail-image"
+          src={item.coverImageUrl}
+          alt="cat"
+        />
+        <div className="project-item-contents">
+          <p className="project-item-title">{item.name}</p>
+          {item.projectTechStacks.map((stack) => (
+            <Chip
+              className="project-item-chip"
+              key={`project-item-${stack.id}`}
+              label={stack.techStack.name}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 

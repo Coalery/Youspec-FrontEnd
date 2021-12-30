fabd8c6b08b9ead6d5204c89466c45d02ed70401
러리 <doralife12@naver.com>
2021-12-30 12:31:33 +0900

    Refactor: Apply edited portfolio data

--- src/modules/portfolio.js
+++ src/modules/portfolio.js
@@ -4,24 +4,24 @@ import {
   handleAsyncActions,
   reducerUtils,
 } from '../lib/asyncUtils';
+import { endEdit } from './portfolio_edit';
 
 const GET_PORTFOLIO = 'PORTFOLIO/GET_PORTFOLIO';
 const GET_PORTFOLIO_SUCCESS = 'PORTFOLIO/GET_PORTFOLIO_SUCCESS';
 const GET_PORTFOLIO_ERROR = 'PORTFOLIO/GET_PORTFOLIO_ERROR';
-
-const SAVE_PORTFOLIO = 'PORTFOLIO/SAVE_PORTFOLIO';
-const SAVE_PORTFOLIO_SUCCESS = 'PORTFOLIO/SAVE_PORTFOLIO_SUCCESS';
-const SAVE_PORTFOLIO_ERROR = 'PORTFOLIO/SAVE_PORTFOLIO_ERROR';
 
 export const getPortfolio = createPromiseThunk(
   GET_PORTFOLIO,
   portfolioApi.getPortfolio
 );
 
-export const savePortfolio = createPromiseThunk(
-  SAVE_PORTFOLIO,
-  portfolioApi.savePortfolio
-);
+export const savePortfolio = (portfolio) => async (dispatch) => {
+  try {
+    await portfolioApi.savePortfolio(portfolio);
+    dispatch(endEdit());
+    dispatch(getPortfolio(portfolio.customName));
+  } catch (e) {}
+};
 
 const initialState = {
   portfolio: reducerUtils.initial(),
@@ -34,10 +34,6 @@ export default function portfolio(state = initialState, action) {
     case GET_PORTFOLIO_SUCCESS:
     case GET_PORTFOLIO_ERROR:
       return handleAsyncActions(GET_PORTFOLIO, 'portfolio')(state, action);
-    case SAVE_PORTFOLIO:
-    case SAVE_PORTFOLIO_SUCCESS:
-    case SAVE_PORTFOLIO_ERROR:
-      return handleAsyncActions(SAVE_PORTFOLIO, 'savePortfolio')(state, action);
     default:
       return state;
   }
